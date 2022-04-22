b82705d321e1f32c2262cdb047fde6a2c6d13c49
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-04-22 06:42:53 +0530

    Time: 889 ms (5.02%), Space: 331.5 MB (5.02%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,23 +1,49 @@
 class MyHashMap {
 public:
-    vector<int> mp;
+    vector<list<pair<int,int>>> mp;
     int size ;
     MyHashMap() {
         size = 1e6 + 1;
         mp.resize(size);
-        fill(mp.begin() , mp.end() , -1);
+    }
+    
+    int hash(int key)
+    {
+        return key%size;
+    }
+    
+    list<pair<int,int>> :: iterator search(int key)
+    {
+        int i = hash(key);
+        list<pair<int,int>> :: iterator it = mp[i].begin();
+        
+        while(it != mp[i].end()){
+            if(it->first == key) return it;
+            it++;
+        }
+        return it;
     }
     
     void put(int key, int value) {
-        mp[key] = value;    
+        int i = hash(key);
+        remove(key);
+        mp[i].push_back({key , value});
     }
     
     int get(int key) {
-        return mp[key];
+        int i = hash(key);
+        list<pair<int,int>> :: iterator it = search(key);
+        
+        if(it == mp[i].end()) return -1;
+        
+        return it->second;
     }
     
     void remove(int key) {
-        mp[key] = -1;
+        int i = hash(key);
+        list<pair<int,int>> :: iterator it = search(key);
+        
+        if(it != mp[i].end()) mp[i].erase(it);
     }
 };
 
