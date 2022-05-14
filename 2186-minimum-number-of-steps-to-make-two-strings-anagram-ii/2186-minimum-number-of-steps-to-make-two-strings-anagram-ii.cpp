f0433482df2207c2a39e911db44f0e46cc62f9f7
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-05-14 07:09:28 +0530

    Time: 128 ms (74.59%), Space: 28.9 MB (64.26%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        vector<int> one(26 , 0) , two(26,0);
+        vector<int> one(26 , 0) ;
         
         for(auto i: s)
             one[i - 'a']++;
         for(auto i: t)
-            two[i - 'a']++;
+            one[i - 'a']--;
         
         int ans = 0;
         for(int i=0 ; i<26 ; i++)
-            ans += (abs(one[i]-two[i]));
+            ans += (abs(one[i]));
         
         return ans;
     }
