195ee926eb03a534a1fbf7a387cc8da66e897e34
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-07-15 07:15:51 +0530

    Time: 46 ms (17.36%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    
+    int dfs(int i , int j , vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int max_area = 0;
-        for(int i = 0; i < grid.size(); i++)
-            for(int j = 0; j < grid[0].size(); j++)
-                if(grid[i][j] == 1)max_area = max(max_area, AreaOfIsland(grid, i, j));
-        return max_area;
+        if(i>=m || j>=n || i<0 || j<0 || grid[i][j] != 1)return 0;
+        grid[i][j] = 'v';
         
+        int up = dfs(i-1 , j , grid);
+        int down = dfs(i+1 , j , grid);
+        int left = dfs(i , j-1 , grid);
+        int right = dfs(i , j+1 , grid);
+        
+        return (up + down + left + right + 1);
     }
     
-    int AreaOfIsland(vector<vector<int>>& grid, int i, int j){
-        if( i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1){
-            grid[i][j] = 0;
-            return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        int  area = 0;
+        for(int i=0 ; i<grid.size() ; ++i)
+        {
+            for(int  j=0 ; j<grid[0].size() ; ++j)
+            {
+                if(grid[i][j] == 1)area = max(area , dfs(i , j , grid));
+            }
         }
-        return 0;
+        
+        return area;
     }
 };
