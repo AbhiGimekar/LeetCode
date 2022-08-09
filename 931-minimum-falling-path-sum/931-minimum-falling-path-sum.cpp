63bc0743412fbab79fbc991efa0ec35290f669de
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-08-09 16:37:34 +0530

    Time: 11 ms (98.09%), Space: 9.9 MB (70.60%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,52 +1,39 @@
 class Solution {
 public:
-    
-    int fun(int i , int j , int n , vector<vector<int>> &mat , vector<vector<int>> &dp)
-    {   
-        if(j<0 || j>=n)return 1e7;
-        if(i == 0)return dp[i][j] =  mat[i][j];
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int up = mat[i][j] + fun(i-1 , j , n , mat ,dp);
-        int ld = mat[i][j] + fun(i-1, j-1 , n ,mat ,dp);
-        int rd = mat[i][j] + fun(i-1 , j+1 , n, mat ,dp);
-        
-        return dp[i][j] =  min(up , min(ld,rd));
-    }
-    
+
     int minFallingPathSum(vector<vector<int>>& mat) {
         
         int n = mat.size();
         
-        vector<vector<int>> dp(n , vector<int>(n,0));
+        vector<int> prev(n,0) , cur(n,0);
         
         for(int j=0 ; j<n ; j++)
-            dp[0][j] = mat[0][j];
+            prev[j] = mat[0][j];
         
         for(int i=1 ; i<n ; i++)
         {
             for(int j=0 ; j<n ; j++)
             {
-                int up = mat[i][j] + dp[i-1][j];
+                int up = mat[i][j] + prev[j];
                 
                 int ld = mat[i][j];
-                if(j-1 >= 0) ld += dp[i-1][j-1];
+                if(j-1 >= 0) ld += prev[j-1];
                 else ld += 1e7;
                 
                 int rd = mat[i][j];
-                if(j+1 < n) rd += dp[i-1][j+1];
+                if(j+1 < n) rd += prev[j+1];
                 else rd += 1e7;
                 
                 
-                dp[i][j] = min(up,min(ld,rd));
+                cur[j] = min(up,min(ld,rd));
             }
+            prev = cur;
         }
         
         
         int mini = INT_MAX;
         for(int j=0 ; j<n ; j++)
-            mini = min(mini , dp[n-1][j]);
+            mini = min(mini , prev[j]);
         
         return mini;
         
