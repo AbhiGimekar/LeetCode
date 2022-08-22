d1a3419d4440744b7a30a10912ce6a6b3da0ede8
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-08-22 06:58:34 +0530

    Time: 426 ms (47.89%), Space: 75.3 MB (86.54%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     
-    int fun(int index , int buy , int rem , int n , vector<int> &prices , vector<vector<vector<int>>> &dp)
-    {
-        
-        if(rem == 0)return 0;
-        if(index == n )return 0;
-        
-        if(dp[index][buy][rem] != -1)return dp[index][buy][rem];
-        
-        if(buy)
-        {
-            return dp[index][buy][rem] = max(-prices[index] + fun(index+1 , 0 , rem , n , prices ,dp) ,
-                                  0   + fun(index+1 , 1 , rem , n , prices,dp));
-        }
-        else
-        {
-            return dp[index][buy][rem] = max(prices[index] + fun(index+1 , 1 , rem-1 , n , prices ,dp), 
-                                   0 + fun(index+1 , 0 , rem , n , prices ,dp));
-        }
-    }
-    
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
         
         
-        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(3 , 0)));
+        vector<vector<int>> after(2 , vector<int>(3 , 0));
+        vector<vector<int>> cur(2 , vector<int>(3 , 0));
+
         
         for(int index = n-1 ; index >= 0 ; index--)
         {
@@ -36,20 +18,21 @@ public:
                 {
                     if(buy)
                     {
-                        dp[index][buy][rem] = max(-prices[index] + dp[index+1][0][rem] ,
-                                                           0     + dp[index+1][1][rem]);
+                        cur[buy][rem] = max(-prices[index] + after[0][rem] ,
+                                                           0     + after[1][rem]);
                     }
                     else
                     {
-                        dp[index][buy][rem] = max(prices[index] + dp[index+1][1][rem-1] ,
-                                                            0   + dp[index+1][0][rem]);
+                        cur[buy][rem] = max(prices[index] + after[1][rem-1] ,
+                                                            0   + after[0][rem]);
                     }
                 }
             }
+            after = cur;
         }
         
         
         
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
