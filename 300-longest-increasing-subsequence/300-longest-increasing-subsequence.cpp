e99f46c930b5f196455a98a2310e90ce891aca94
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-08-25 07:05:01 +0530

    Time: 2023 ms (5.98%), Space: 292.3 MB (8.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,26 +3,23 @@ public:
     
     int fun(int index , int prev , vector<int> &nums , vector<vector<int>> &dp)
     {
-        if(index == nums.size()+1) return 0;
+        if(index == nums.size()) return 0;
         
-        if(dp[index][prev] != -1)return dp[index][prev]; 
+        if(dp[index][prev+1] != -1) return dp[index][prev+1];
         
-        int notTake = 0 + fun(index+1 , prev , nums ,dp);
-        int take = -1e9;
-        if(prev == 0 || nums[index-1] > nums[prev-1])
-            take = 1 + fun(index+1 , index , nums ,dp);
+        int notTake = 0 + fun(index+ 1, prev , nums ,dp);
+        int take = 0;
+        if(prev == -1 || nums[index] > nums[prev])
+            take = 1 + fun(index+1 , index , nums , dp);
         
-        return dp[index][prev] = max(take , notTake);
+        return dp[index][prev+1] = max(take , notTake);
     }
     
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
+        vector<vector<int>> dp(n , vector<int>(n+1,-1));
         
-        vector<vector<int>>  dp(n+1 , vector<int>(n ,-1));
-        
-        return fun(1 , 0 , nums ,dp);
-        
-        
+        return fun(0, -1 , nums ,dp);
     }
 };
