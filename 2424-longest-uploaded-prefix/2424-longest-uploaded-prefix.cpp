4337f0ae32db95ae3b3b384797bd65303d21b399
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-10-02 06:00:40 +0530

    Time: 757 ms (20.00%), Space: 175.2 MB (20.00%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
@@ -0,0 +1,24 @@
+class LUPrefix {
+    unordered_set<int>s;
+    int last=0;
+public:
+    LUPrefix(int n) {
+        
+    }
+    
+    void upload(int video) {
+        s.insert(video);
+        while(s.count(last+1))last++;
+    }
+    
+    int longest() {
+        return last;
+    }
+};
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix* obj = new LUPrefix(n);
+ * obj->upload(video);
+ * int param_2 = obj->longest();
+ */
