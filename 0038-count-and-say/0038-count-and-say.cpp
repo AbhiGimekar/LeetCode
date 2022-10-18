334bc39391a0c890f14601df47fd43188af38d36
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-10-18 06:42:40 +0530

    Time: 9 ms (74.27%), Space: 6.5 MB (77.40%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        string curr_str;
+        curr_str += '1';
+        
+        for (int i = 0; i < n - 1; i++)
+        {
+            string buffer;
+
+            int index = 0;
+            for (int index = 0; index < curr_str.size(); ++index)
+            {
+                int cnt = 1; 
+
+                while (index + 1 < curr_str.size() and curr_str[index + 1] == curr_str[index]) 
+                {
+                    index++;
+                    cnt++;
+                }
+
+                buffer.push_back(cnt + '0');
+                buffer.push_back(curr_str[index]);
+            }
+
+            curr_str = buffer;
+        }
+        
+        return curr_str;
+    }
+};
