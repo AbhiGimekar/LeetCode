6d6abcb2971f20f853d6e5fcb7447a895f25d6d0
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-10-23 06:44:39 +0530

    Time: 87 ms (42.71%), Space: 22.2 MB (47.16%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,20 +3,16 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         
         int dup = -1, missing = 1;
-        unordered_map<int,int> mp;
+        vector<int> mp(nums.size()+1,0);
+        
         for(auto i:nums)mp[i]++;
         
         for(int i=0 ; i<=nums.size(); i++)
         {
-            if(mp.find(i) != mp.end())
-            {
-                if(mp[i] == 2)
-                    dup = i;
-            }
-            else
-            {
+            if(mp[i] == 0)
                 missing = i;
-            }
+            else if(mp[i] == 2)
+                dup = i;
         }
         
         return {dup,missing};
