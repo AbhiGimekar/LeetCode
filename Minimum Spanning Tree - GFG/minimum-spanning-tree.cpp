cbb2c2b22ea18586ca049e1439b9e4393f7ee04d
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-11-07 08:05:20 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,38 +3,101 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParent_u = findParent(u);
+        int ultimateParent_v = findParent(v);
+
+        if (ultimateParent_u == ultimateParent_v)
+            return;
+        else if (rank[ultimateParent_u] < rank[ultimateParent_v])
+            parent[ultimateParent_u] = ultimateParent_v;
+        else if (rank[ultimateParent_v] < rank[ultimateParent_u])
+            parent[ultimateParent_v] = ultimateParent_u;
+        else
+        {
+            parent[ultimateParent_u] = ultimateParent_v;
+            rank[ultimateParent_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParent_u = findParent(u);
+        int ultimateParent_v = findParent(v);
+
+        if (ultimateParent_u == ultimateParent_v)
+            return;
+        else if (size[ultimateParent_u] < size[ultimateParent_v])
+        {
+            parent[ultimateParent_u] = ultimateParent_v;
+            size[ultimateParent_v] += size[ultimateParent_u];
+        }
+        else
+        {
+            parent[ultimateParent_v] = ultimateParent_u;
+            size[ultimateParent_u] += size[ultimateParent_v];
+        }
+    }
+};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // code here
-        int cost = 0;
-        int vis[V] = {0};
-        
-        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>> > pq;
-        pq.push({0,0}); // {weight, node}
-        
-        while(!pq.empty())
-        {
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            
-            if(vis[node] == 1)continue;
-            vis[node] = 1;
-            cost += wt;
-            
-            for(auto i : adj[node]){
-                int adjNode = i[0];
-                int edgeWt = i[1];
-                if(!vis[adjNode]){
-                    pq.push({edgeWt, adjNode});
-                }
+        vector<pair<int, pair<int,int>>> edges;
+        for(int i=0; i<V; i++){
+            for(auto j : adj[i]){
+                int adjNode = j[0];
+                int wt = j[1];
+                int node = i;
+                
+                edges.push_back({wt, {node, adjNode}});
             }
         }
         
+        DisjointSet ds(V);
+        sort(begin(edges), end(edges));
+        int cost = 0;
+        
+        for(auto i : edges){
+            int wt = i.first;
+            int u = i.second.first;
+            int v = i.second.second;
+            
+            if(ds.findParent(u) != ds.findParent(v)){
+                cost += wt;
+                ds.unionBySize(u, v);
+            } 
+        }
         return cost;
     }
 };
