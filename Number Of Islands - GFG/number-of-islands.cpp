2c3ce1b2a9f2606250d3a9f585eab7ede83ae8a8
AbhiGimekar <96155838+AbhiGimekar@users.noreply.github.com>
2022-11-09 06:52:59 +0530

    Added solution - LeetHub

---
+++ Number Of Islands - GFG/number-of-islands.cpp
@@ -0,0 +1,147 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParent_u = findParent(u);
+        int ultimateParent_v = findParent(v);
+
+        if (ultimateParent_u == ultimateParent_v)
+            return;
+        else if (rank[ultimateParent_u] < rank[ultimateParent_v])
+            parent[ultimateParent_u] = ultimateParent_v;
+        else if (rank[ultimateParent_v] < rank[ultimateParent_u])
+            parent[ultimateParent_v] = ultimateParent_u;
+        else
+        {
+            parent[ultimateParent_u] = ultimateParent_v;
+            rank[ultimateParent_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParent_u = findParent(u);
+        int ultimateParent_v = findParent(v);
+
+        if (ultimateParent_u == ultimateParent_v)
+            return;
+        else if (size[ultimateParent_u] < size[ultimateParent_v])
+        {
+            parent[ultimateParent_u] = ultimateParent_v;
+            size[ultimateParent_v] += size[ultimateParent_u];
+        }
+        else
+        {
+            parent[ultimateParent_v] = ultimateParent_u;
+            size[ultimateParent_u] += size[ultimateParent_v];
+        }
+    }
+};
+
+class Solution {
+  public:
+    vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
+        // code here
+        
+        DisjointSet ds(n*m);
+        int vis[n][m];
+        memset(vis, 0, sizeof(vis));
+        
+        int cnt = 0;
+        vector<int> ans;
+        
+        for(auto it : operators){
+            int row = it[0];
+            int col = it[1];
+            if(vis[row][col] == 1){
+                ans.push_back(cnt);
+                continue;
+            }
+            vis[row][col] = 1;
+            cnt++;
+            
+            int delRow[] = {-1, 0, 1, 0};
+            int delCol[] = {0, 1, 0, -1};
+            
+            for(int i=0; i<4; i++){
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    if(vis[nrow][ncol] == 1){
+                        int nodeNo = row * m + col;
+                        int adjNodeNo = nrow * m + ncol;
+                        
+                        if(ds.findParent(nodeNo) != ds.findParent(adjNodeNo)){
+                            cnt--;
+                            ds.unionBySize(nodeNo, adjNodeNo);
+                        }
+                    }
+                }
+            }
+            ans.push_back(cnt);
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m,k; cin>>n>>m>>k;
+        vector<vector<int>> a;
+        
+        for(int i=0; i<k; i++){
+            vector<int> temp;
+            for(int j=0; j<2; j++){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            a.push_back(temp);
+        }
+    
+        Solution obj;
+        vector<int> res = obj.numOfIslands(n,m,a);
+        
+        for(auto x : res)cout<<x<<" ";
+        cout<<"\n";
+    }
+}
+
+// } Driver Code Ends
